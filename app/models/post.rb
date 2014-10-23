66779a4f8672fb41c74ccc3fa6419ac815cdcf57
Seth Krasnianski <sethkrasnianski@gmail.com>
2014-10-23 15:15:52 -0400

    Add slugify action

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :slug, :body, :excerpt, :content
   validates :body, :title, presence: true
+
+  def slugify
+    t = Array.new
+    t[] = "Testing"
+    t[] = "Something"
+    t[] = "else"
+  end
 end
